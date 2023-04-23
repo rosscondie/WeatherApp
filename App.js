65539d146b5e93b91639bec767af075fe4ad2889
Ross Condie <rosscondie@gmail.com>
2023-04-23 12:37:53 +0200

    refactored tab components into Tabs components

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import CurrentWeather from './src/screens/CurrentWeather';
-import UpcomingWeather from './src/screens/UpcomingWeather';
-import City from './src/screens/City';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Tab = createBottomTabNavigator();
+import { NavigationContainer } from '@react-navigation/native';
+import Tabs from './src/components/Tabs';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name={'Current'} component={CurrentWeather} />
-        <Tab.Screen name={'Upcoming'} component={UpcomingWeather} />
-        <Tab.Screen name={'City'} component={City} />
-      </Tab.Navigator>
+      <Tabs />
     </NavigationContainer>
   );
 };
